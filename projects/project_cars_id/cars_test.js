0afe111d8a740947377b7cce46d44affd6f8bc0e
krotok <kkonstant@gmail.com>
2025-07-02 22:57:50 +0300

    123

--- projects/project_cars_id/cars_test.js
+++ projects/project_cars_id/cars_test.js
@@ -4,11 +4,11 @@ import { check, sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 
 export let options = {
-    vus: 50,
-    duration: '20s',
+    vus: 1,
+    duration: '10s',
 };
 
-let cars = Array.from({ length: 1500 }, (_, i) => i + 1);
+let cars = Array.from({ length: 20 }, (_, i) => i + 1);
 
 let successResponses = [];
 let totalTime = 0;
@@ -17,12 +17,19 @@ let totalOk = 0;
 export default function () {
     let index = Math.floor(Math.random() * cars.length);
     let carNumber = cars[index];
-    let res = http.get(`https://host.com/api/car_${carNumber}`);
+    //let res = http.get(`http://127.0.0.1/app/car_${carNumber}`);
+    let res = http.get(`http://localhost/app/car_11`);
 
-    if (res.status === 200 && res.body && res.body !== '{}' && res.json('status') === true) {
-        successResponses.push(res.timings.duration);
-        totalTime += res.timings.duration;
-        totalOk += 1;
+    console.log(`RAW body: ${res.body}`);
+    if (res.status === 200 && res.body && res.body !== '{}') {
+        let status = res.json('status');
+        let returnedCarNumber = res.json('car_number');
+        console.log(`car_number: ${returnedCarNumber}, status: ${status}`);
+
+        if (status === true) {
+            successRT.add(res.timings.duration);
+            okCount.add(1);
+        }
     }
 
     sleep(0.1); // контроль паузы между запросами, можно удалить
@@ -31,7 +38,7 @@ export default function () {
 export function handleSummary(data) {
     const avg = totalOk > 0 ? totalTime / totalOk : 0;
     return {
-        stdout: `\nСреднее время отклика для status:true: ${avg.toFixed(2)} ms из ${totalOk} успешных запросов\n`,
+        stdout: `\nAverage response time  for satus:true: ${avg.toFixed(2)} ms from ${totalOk} succed requests\n`,
         'results.json': JSON.stringify(data),
     };
 }
